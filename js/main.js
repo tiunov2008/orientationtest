49582a581a69bd797e84d8c8663d15ab027fb33a
Tiunov Vladimir <tiunov2008.va@gmail.com>
2024-06-16 19:57:34 +0300

    ac

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //        element.style.transform = `rotate3d(1, 1, 1, ${event.clientX*event.clientY/1000}deg)`
     //    });
     //});
-    window.addEventListener('orientationchange',(e)=>{
+    window.addEventListener('devicemotion',(e)=>{
+        console.log(e.acceleration.z);
         [...document.querySelectorAll('.card')].forEach(element => {
-        element.style.transform = `rotateZ(${e.target.screen.orientation.angle}deg)`
+        element.style.transform = `rotateZ(${e.acceleration.z}deg)`
         });
     })
 })
