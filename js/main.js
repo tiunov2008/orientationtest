3b7f7a4d47316c66d7311606817f6726285e028c
Tiunov Vladimir <tiunov2008.va@gmail.com>
2024-06-19 10:58:08 +0300

    25

--- js/main.js
+++ js/main.js
@@ -4,18 +4,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
 
     function onOrientationChange(e) {
-        // покажем значения параметров в реальном времени
-        document.getElementById('alpha').innerHTML = 'α = ' + Math.round(e.alpha);
-        document.getElementById('beta').innerHTML = 'β = '+Math.round(e.beta);
-        document.getElementById('gamma').innerHTML = 'γ = '+Math.round(e.gamma);
-         // поворот и масштабирование квадрата 
-         // в зависимости от ориентации экрана
-        var rotate = 'rotate(' + e.gamma + 'deg)';
-        var scale = 'scale(' + ((e.beta/180)*2 + 1) + ')';
         [...document.querySelectorAll('.card')].forEach(element => {
-            element.style.transform = `rotate3d(${Math.round(e.alpha)}, ${Math.round(e.beta)}, ${Math.round(e.gamma)}, 1deg)`
+            element.style.transform = `rotateZ(${Math.round(e.alpha)}deg)`
         });
-        document.getElementById('a').style.webkitTransform = rotate+' '+scale;
     }
     
     window.addEventListener('deviceorientation', onOrientationChange);
