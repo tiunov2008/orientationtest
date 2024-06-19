24ea77343fa0db3a623e01cf96a7bbea7f9f0477
Tiunov Vladimir <tiunov2008.va@gmail.com>
2024-06-19 10:49:20 +0300

    45

--- js/main.js
+++ js/main.js
@@ -7,10 +7,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //        element.style.transform = `rotate3d(1, 1, 1, ${event.clientX*event.clientY/1000}deg)`
     //    });
     //});
-    window.addEventListener('devicemotion',(e)=>{
-        document.querySelector('h1').innerHTML = e.acceleration.x;
-        [...document.querySelectorAll('.card')].forEach(element => {
-        element.style.transform = `rotateZ(${e.acceleration.x}deg)`
-        });
-    })
+    function onOrientationChange(e) {
+        // покажем значения параметров в реальном времени
+        document.getElementById('alpha').innerHTML = 'α = ' + Math.round(e.alpha);
+        document.getElementById('beta').innerHTML = 'β = '+Math.round(e.beta);
+        document.getElementById('gamma').innerHTML = 'γ = '+Math.round(e.gamma);
+         // поворот и масштабирование квадрата 
+         // в зависимости от ориентации экрана
+         var rotate = 'rotate(' + e.gamma + 'deg)';
+         var scale = 'scale(' + ((e.beta/180)*2 + 1) + ')';
+         document.getElementById('a').style.webkitTransform = rotate+' '+scale;
+       }
+     
+       window.addEventListener('deviceorientation', onOrientationChange);
 })
